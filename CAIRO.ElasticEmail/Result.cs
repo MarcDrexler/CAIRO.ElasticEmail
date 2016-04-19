54f8076a2cb7fb2cc0228040e0f0e64b1bc725cf
Carlos Jiménez <carlos@amillionmonkeys.es>
2016-04-19 12:19:12 +0200

    Updated GetDeliveryStatus to return more data

---
+++ CAIRO.ElasticEmail/Result.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CAIRO.ElasticEmail
+{
+    public class SendResult
+    {
+        public ResultType ResultType { get; set; }
+        public string ErrorMessage { get; set; }
+        public Guid? TransactionId { get; set; }
+    }
+}
